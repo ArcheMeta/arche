b0c5603381893fe40367fd737b4f4d27bf070581
happy955 <596155717@qq.com>
2022-05-10 18:19:58 +0800

    404

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 window.addEventListener('load', function () {
+  /* setTimeout(function(){
+    window.location.href = '404.html'
+  }) */
   document.getElementById("preloader").classList.add("fadehide");
 })
 
